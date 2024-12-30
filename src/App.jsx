2f5e16b1f78f0389ec609ae822c560516329a1b1
J-Lee12 <joshualee2022.1@u.northwestern.edu>
2024-12-30 10:39:36 -0600

    added grid as well as well as added new portion into bottom part of website

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from "./navbar";
 import Typography from "@mui/material/Typography";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
+import Grid from "@mui/material/Grid2";
 
 import "./App.css";
 
@@ -12,13 +13,27 @@ function App() {
   return (
     <>
       <Navbar />
-      <div className="flex items-center justify-center mt-56">
-        <Card className="p-4 border-none shadow-none w-1/4">
-          <CardContent>
-            <Typography variant="h2">We Don't Change, but Evolve</Typography>
-          </CardContent>
-        </Card>
-      </div>
+      <Grid container direction="column">
+        <Grid item xs={12} sm={12}>
+          <div className="flex items-center justify-center mt-56">
+            <Card className="p-4 border-none shadow-none w-1/4">
+              <CardContent>
+                <Typography variant="h2">
+                  We Don't Change, but Evolve
+                </Typography>
+              </CardContent>
+            </Card>
+          </div>
+        </Grid>
+        <Grid item xs={12} sm={12}>
+          <Card elevation={0}>
+            <CardContent>
+              <Typography variant="h4">We Provide</Typography>
+              <Typography variant="body1"> Example text</Typography>
+            </CardContent>
+          </Card>
+        </Grid>
+      </Grid>
     </>
   );
 }
